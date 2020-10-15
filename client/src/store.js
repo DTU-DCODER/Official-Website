44783433e84dd3be7c4f2f7c924d7d918be7daa0
Kushagra <kush12092001@gmail.com>
2020-10-15 20:56:07 +0530

    Optimized for node v14+

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+/*import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -12,6 +12,22 @@ const store = createStore(
     compose(applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
+);
+*/
+
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
