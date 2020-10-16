3c981e1f21d511e002e5a0da589ee48529e113a9
seneark <seneark@protonmail.com>
2020-10-16 13:12:02 +0530

    changed Readme

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,12 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
 );
+
 
 export default store;
