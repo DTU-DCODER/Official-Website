d8a16b630f3f0d48ab81e9427c64b6292e415e20
Anurag5086 <anuraggupta5086@gmail.com>
2020-10-23 19:17:03 +0530

    fixed sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,36 +1,34 @@
 import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import { IconContext } from 'react-icons';
+import { BsTextIndentLeft } from "react-icons/bs";
+
 import './Sidebar.css';
 
 function Sidebar() {
-    const [sidebar, setSidebar] = useState(false);
+    const [sidebar, setSidebar] = useState(true);
     const showSidebar = () => setSidebar(!sidebar);
 
     return (
         <>
             <div className='sidebar'>
-                <Link to='#' className='menu-bars'>
+                <Link to='#' className='menu-bar-inactive'>
                     <IconContext.Provider value={{ color: '#252537' }}>
-                        <FaIcons.FaBars onClick={showSidebar} />
+                        <BsTextIndentLeft onClick={showSidebar} style={{fontSize: '35px'}}/>
                     </IconContext.Provider>
                 </Link>
             </div>
             <IconContext.Provider value={{ color: 'white' }}>  
                 <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                    <ul className='nav-menu-items' onClick={showSidebar}>
-                        <li className='sidebar-toggle'>
-                            <Link to='#' className='menu-bars'>
-                                <AiIcons.AiOutlineClose />
+                            <Link to='#' className='menu-bars' onClick={showSidebar}>
+                                <BsTextIndentLeft />
                             </Link>
-                        </li>
+                    <ul className='nav-menu-items'>
                         {SidebarData.map((item, index) => {
                             return (
                                 <li key={index} className={item.cName}>
-                                    <Link to={item.path}>
+                                    <Link to={item.path} >
                                         {item.icon}
                                         <span>{item.title}</span>
                                     </Link>
