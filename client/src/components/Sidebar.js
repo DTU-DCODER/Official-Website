0522a0d37a0c040c641f8c4e0a39698d0766aba4
thanapoom21 <thanapoom.p.music@gmail.com>
2020-10-30 00:36:27 -0700

    add searchbar, svg icons, and lift up sidebar state

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import { IconContext } from 'react-icons';
@@ -6,22 +6,20 @@ import { BsTextIndentLeft,BsTextIndentRight } from "react-icons/bs";
 
 import './Sidebar.css';
 
-function Sidebar() {
-    const [sidebar, setSidebar] = useState(true);
-    const showSidebar = () => setSidebar(!sidebar);
+function Sidebar(props) {
 
     return (
         <>
             <div className='sidebar'>
                 <Link to='#' className='menu-bar-inactive'>
                     <IconContext.Provider value={{ color: '#252537' }}>
-                        <BsTextIndentLeft onClick={showSidebar} />
+                        <BsTextIndentLeft onClick={props.sidebarState} />
                     </IconContext.Provider>
                 </Link>
             </div>
             <IconContext.Provider value={{ color: 'white' }}>
-                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                            <Link to='#' className='menu-bars' onClick={showSidebar}>
+                <nav className={props.initialState ? 'nav-menu active' : 'nav-menu'}>
+                            <Link to='#' className='menu-bars' onClick={props.sidebarState}>
                                 <BsTextIndentRight />
                             </Link>
                     <ul className='nav-menu-items'>
